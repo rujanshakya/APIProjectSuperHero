6f5ed5241c1e99f54efe6a11eef84cd3267ff6f0
RujanShakya <rujan.shakya123@gmail.com>
2022-09-06 11:51:16 +0545

    Add project files.

---
+++ APIProject/Controllers/SuperHeroController.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APIProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SuperHeroController : ControllerBase
+    {
+        private static List<SuperHero> heroes = new List<SuperHero>
+            {
+            new SuperHero{ Id=1,
+                Name="Spider-Man",
+                FirstName="Peter",
+                LastName="Parker",
+                Place="Queens"},
+
+            new SuperHero{ Id=2,
+                Name="Iron-Man",
+                FirstName="Tony ",
+                LastName="Stark",
+                Place="LongiLand"},
+           };
+
+        private DataContext _context;
+
+        public SuperHeroController(DataContext context) { 
+        _context=context;
+        }
+
+
+
+        [HttpGet]
+        public async Task<ActionResult<List<SuperHero>>> Get()
+        {
+
+            return Ok(await _context.SuperHeroes.ToListAsync());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<SuperHero>> GetHeroById(int id) {
+            var hero =  _context.SuperHeroes.FindAsync(id);
+            if (hero == null)
+            {
+                return BadRequest("Hero not found");
+            }
+            else
+            {
+                return Ok(hero);
+            
+            }
+        }
+        [HttpPost]
+        public async Task<ActionResult<List<SuperHero>>> AddHero(SuperHero hero) {
+            _context.SuperHeroes.Add(hero);
+            await _context.SaveChangesAsync();
+            return Ok(await _context.SuperHeroes.ToListAsync());
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<List<SuperHero>>> Updatesuperhero(SuperHero request)
+        {
+            var hero = await _context.SuperHeroes.FindAsync(request.Id);
+            if (hero == null)
+            {
+                return BadRequest("Hero not found");
+            }
+            else
+            {
+                hero.Name = request.Name;
+                hero.FirstName = request.FirstName;
+                hero.LastName = request.LastName;
+                hero.Place=request.Place;
+                await _context.SaveChangesAsync();
+                return Ok(await _context.SuperHeroes.ToListAsync());
+            }
+        }
+        [HttpDelete]
+        public async Task<ActionResult<List<SuperHero>>> DeleteHero(int id) {
+            var dbhero = await _context.SuperHeroes.FindAsync(id);
+            if (dbhero == null) {
+                return BadRequest("Id not match");
+            }
+            else
+            {
+                _context.SuperHeroes.Remove(dbhero);
+                await _context.SaveChangesAsync();
+
+            }
+            return Ok(await _context.SuperHeroes.ToListAsync());
+        }
+
+
+    }
+}
---
+++ APIProject/Data/DataContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Security.Cryptography.X509Certificates;
+
+namespace APIProject.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+            public DbSet<SuperHero> SuperHeroes { get; set; }
+    
+        
+    }
+}
